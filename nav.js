51066d8a4ba5aa6a37e582835036423ef21ceaa9
RosaBen <benchabane.rosa@outlook.com>
2025-09-28 18:39:22 +0200

    fixed css navbar

--- nav.js
+++ nav.js
@@ -3,61 +3,72 @@ import { images, listPages } from './data.js';
 
 const navbar = document.createElement('nav');
 const logo = document.createElement('img');
-const div = document.createElement('div');
-const menu = document.createElement('a');
+const desktopLinks = document.createElement('div');
+const menuBtn = document.createElement('button');
 const iconMenu = document.createElement('i');
+const mobileMenu = document.createElement('div');
+
+
 listPages.forEach(nav => {
   const link = document.createElement('a');
   const icon = document.createElement('i');
-  const listNav = document.createElement('div');
+  const navItem = document.createElement('div');
+
   link.textContent = nav.page;
   const match = images.find(item => nav.href.includes(item.name));
   if (match && match.icon) {
     icon.classList.add('fa-solid', match.icon);
   }
+
   link.classList.add('page');
   link.id = nav.href;
   link.setAttribute("data-page", nav.href);
   link.href = "#";
-  listNav.classList.add('nav-item');
-  listNav.appendChild(icon);
-  listNav.appendChild(link);
-  div.appendChild(listNav);
+
+  navItem.classList.add('nav-item');
+  navItem.appendChild(icon);
+  navItem.appendChild(link);
+
+  // version desktop
+  desktopLinks.appendChild(navItem);
+
+  // version mobile
+  const mobileNavItem = navItem.cloneNode(true);
+  mobileMenu.appendChild(mobileNavItem);
 });
 
 if (images[6] && images[6].icon) {
   iconMenu.classList.add('fa-solid', images[6].icon);
 }
-menu.appendChild(iconMenu);
-menu.classList.add('menu');
-div.classList.add('links-container');
+menuBtn.appendChild(iconMenu);
+menuBtn.classList.add('menu-button');
+desktopLinks.classList.add('desktop-links');
+mobileMenu.classList.add('mobile-menu', 'hidden');
 if (images[2]) {
   logo.src = images[2].src;
   logo.alt = images[2].name;
 }
+
 navbar.appendChild(logo);
-navbar.appendChild(menu);
-const toggleMenu = () => {
-  const menuContainer = document.createElement('div');
-  menuContainer.classList.add('menu-container');
-  div.classList.toggle('active');
-  menuContainer.appendChild(menu);
-  menuContainer.appendChild(div);
-  navbar.appendChild(menuContainer);
-};
+navbar.appendChild(desktopLinks);
+navbar.appendChild(menuBtn);
+navbar.appendChild(mobileMenu);
+
+
 const displayMenu = () => {
   if (window.innerWidth < 900) {
-    if (div) div.classList.add('hidden');
-    if (menu) menu.classList.remove('hidden');
-    menu.addEventListener('click', toggleMenu);
+    desktopLinks.classList.add('hidden');
+    menuBtn.classList.remove('hidden');
   } else {
-    if (menu) menu.classList.add('hidden');
-    if (div) div.classList.remove('hidden');
-    navbar.appendChild(div);
+    desktopLinks.classList.remove('hidden');
+    menuBtn.classList.add('hidden');
+    mobileMenu.classList.add('hidden');
   }
 };
 
-displayMenu();
+menuBtn.addEventListener('click', function () {
+  mobileMenu.classList.toggle('hidden');
+});
 
 
 
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import { navbar, displayMenu } from "./nav.js";
 
 window.addEventListener('resize', displayMenu);
+displayMenu();
 const header = document.querySelector('header');
 header.appendChild(navbar);
 
